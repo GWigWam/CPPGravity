df9199f49ecfc6d5b4053c0c5447b5983bf4d885
GWigWam <welwisam@gmail.com>
2019-02-12 20:43:32 +0100

    Setup SFML

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main() {
-    std::cout << "Hello world!" << std::endl;
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML");
+    sf::Clock clock;
+
+    while(window.isOpen()) {
+        sf::Event event;
+        while(window.pollEvent(event)) {
+            if(event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+
+        sf::Time elapsed = clock.restart();
+
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Escape)) {
+            window.close();
+        }
+
+        window.clear();
+        window.display();
+    }
+
+
     return 0;
 }
