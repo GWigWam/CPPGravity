d6039b8374b7692a2fc818fae55c79120419625e
GWigWam <welwisam@gmail.com>
2019-02-21 21:59:25 +0100

    Setup moon<earth<sun sim

--- Context.cpp
+++ Context.cpp
@@ -14,8 +14,17 @@ void Context::init(unsigned int noObjs, unsigned long size, float timeFactor) {
         Entities.push_back(Entity(x, y, 0, 0, 1000000000));
     }*/
 
-    Entities.push_back(Entity(500000, 500000, 0, 0, 1988500000000000000/*000000000000*/));                // sun      1.9885e30kg
-    Entities.push_back(Entity(500000 - 147800/*000000*/, 500000, 0, 0, 5972300000000/*000000000000*/));   // earth    5.9723e24kg, 1.478e11m dist, 30140m/s
+    auto center = size / 2;
+    Entities.push_back(Entity(center, center, 0, 0, 1988500000000000000.000000000000));         // sun      1.9885e30kg
+    auto earth_apo = 152100;
+    auto earth_x = center - earth_apo;
+    auto earth_apo_spd = 0.02929;
+    Entities.push_back(Entity(earth_x, center, 0, earth_apo_spd, 5972300000000.000000000000));  // earth    5.9723e24kg, 1.496e11m dist,  30140m/s
+    auto moon_dist = 384.402;
+    auto moon_x = earth_x - moon_dist;
+    auto moon_spd_e = 0.001022;
+    auto moon_spd = moon_spd_e + earth_apo_spd;
+    Entities.push_back(Entity(moon_x, center, 0, moon_spd, 73420000000.000000000000));          // moon     7.342e22kg,  3.84402e8m dist, 1022m/s
 }
 
 void Context::update(const float& elapsedSec) {
